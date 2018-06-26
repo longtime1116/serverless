9950c1bbc294003e060b5f2eb14819cc4fbf59d2
shumo.tokinaga <longtime1116@gmail.com>
2018-06-26 18:39:58 +0900

    add invoke_hello function

--- myService/handler.js
+++ myService/handler.js
@@ -1,10 +1,24 @@
 'use strict';
 
+module.exports.invoke_hello = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!(invoke hello)',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
 module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
+      message: 'Go Serverless v1.0! Your function executed successfully!(hello)',
       input: event,
     }),
   };
